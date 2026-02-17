e3e9703e6633fd94e20aaf9025a6d2b71d8fc4ef
muhozadenyse9 <muhozadenyse9@gmail.com>
2026-02-17 15:03:04 +0200

    created index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// erxercise 1: Arrays initianilization and property access//
+const productcategories = ["Books", "Electronics", "Clothing", "Home & Kitchen", "toys & Games"];
+// a
+console.log(productcategories[0]); // Output: Electronics
+// b
+console.log(productcategories[productcategories.length -1]); // Output: toys & Games
+// c
+console.log(productcategories.length); // Output: 5
+// d
+productcategories[1] = "Sports & Outdoors";
+console.log(productcategories); // Output: ["Books", "Sports & Outdoors", "Clothing", "
+// e
+console.log(productcategories[2]); // Output: Clothing
+ 
