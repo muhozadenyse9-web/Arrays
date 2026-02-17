7b605832d8158de734a9cac3abb047c6f838babc
muhozadenyse9 <muhozadenyse9@gmail.com>
2026-02-17 15:11:04 +0200

    did exercise 2

--- index.js
+++ index.js
@@ -11,4 +11,17 @@ productcategories[1] = "Sports & Outdoors";
 console.log(productcategories); // Output: ["Books", "Sports & Outdoors", "Clothing", "
 // e
 console.log(productcategories[2]); // Output: Clothing
- 
+//  errcise 2: Push , Pop, Shift, unshift, Splice
+let inventory =[10,20,30];
+// a
+inventory.push(40);
+console.log(inventory);
+// b
+inventory.pop();
+console.log(inventory);
+// c
+inventory.unshift(5);
+console.log(inventory);
+// d
+inventory.shift();
+console.log(inventory);
